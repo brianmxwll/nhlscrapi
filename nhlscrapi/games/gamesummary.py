6c9e21034b2a5046bf151a2ea678486e7a40140b
Brian Maxwell <brianmxwll@gmail.com>
2015-11-11 10:14:18 -0500

    Added basic goalie summary parsing
    Initial commit towards game summary parsing - adding in goalies. I'm not
    sure what the end goal of this library might be, so I will likely move
    onto other items before continuing the parsing of this page.
    
    Much of it might be (or certainly is) duplicated on other reports.
    Should overlapping data in Event Summary and Game Summary be present in
    both game.event_summary and game.game_summary? Should we merge the two
    and move to just game.summary? Plenty of options and I'll wait for that
    before moving too far forward.

--- nhlscrapi/games/gamesummary.py
+++ nhlscrapi/games/gamesummary.py
@@ -13,5 +13,48 @@ class GameSummary(RepScrWrap):
 
         self._summary_rep = GameSummRep(game_key)
 
+        # Just tell it to parse for now, activate relevant code.
+        self._summary_rep.parse()
+
         #  @RepScrWrap.read_banner('_roster_rep')
         #  @RepScrWrap.lazy_load('_roster_rep', 'parse_rosters')
+
+    @property
+    def away_goalies(self):
+        """
+        Property that returns the away team goalies ..
+        :returns: dict of the form
+        .. code:: python
+            {
+                num: {
+                    'ga': { 'per1/per2/per3/ot/tot': value },
+                    'name': { 'first': first, 'last': last },
+                    'pos': 'G'
+                    'result': value,
+                    'toi': {
+                        'tot/pp/sh/ev': { 'min': mins, 'sec': secs }
+                    }
+                }
+            }
+        """
+        return self._summary_rep.goalie_summary['away']
+
+    @property
+    def home_goalies(self):
+        """
+        Property that returns the home team goalies ..
+        :returns: dict of the form
+        .. code:: python
+            {
+                num: {
+                    'ga': { 'per1/per2/per3/ot/tot': value },
+                    'name': { 'first': first, 'last': last },
+                    'pos': 'G'
+                    'result': value,
+                    'toi': {
+                        'tot/pp/sh/ev': { 'min': mins, 'sec': secs }
+                    }
+                }
+            }
+        """
+        return self._summary_rep.goalie_summary['home']
--- nhlscrapi/scrapr/gamesummrep.py
+++ nhlscrapi/scrapr/gamesummrep.py
@@ -5,14 +5,34 @@ from nhlscrapi.games.playbyplay import Strength
 from nhlscrapi.scrapr.reportloader import ReportLoader
 from nhlscrapi.scrapr.teamnameparser import team_abbr_parser
 
-
 class GameSummRep(ReportLoader):
     """Retrieve and load game summary report from nhl.com"""
 
     def __init__(self, game_key):
         super(GameSummRep, self).__init__(game_key, 'game_summary')
 
+        self.goalie_summary = {'away': {}, 'home': {}}
+        """
+        Property that returns the home/away team goalies.
+        :returns: dict of the form
+        .. code:: python
+            {
+                'home/away': {
+                    num: {
+                        'ga': { 'per1/per2/per3/ot/tot': value },
+                        'name': { 'first': first, 'last': last },
+                        'pos': 'G'
+                        'result': value,
+                        'toi': {
+                            'tot/pp/sh/ev': { 'min': mins, 'sec': secs }
+                        }
+                    }
+                }
+            }
+        """
+
     def parse(self):
+        # Brian TBD: does not match other parse methods, success should return self, None otherwise.
         """Fully parses game summary report.
         :returns: boolean success indicator
         :rtype: bool """
@@ -20,6 +40,7 @@ class GameSummRep(ReportLoader):
         r = super(GameSummRep, self).parse()
         try:
             self.parse_scoring_summary()
+            self.parse_goaltender_summary()
             return r and False
         except:
             return False
@@ -30,10 +51,10 @@ class GameSummRep(ReportLoader):
         main = lx_doc.xpath('//*[@id="MainTable"]')[0]
         scr_summ = main.xpath('child::tr[4]//tr')
         for r in scr_summ:
-            print r.get('class')
+            #print r.get('class')
             if r.get('class') in ['oddColor', 'evenColor']:
                 tds = r.xpath('./td')
-                scr = [td.xpath('text()') for td in tds[:8]]
+                scr = [td.xpath('text()') for td in tds[:8]] #Track for later - can add [0] index inside to avoid using it below
 
                 # goal summry data
                 goals = {}
@@ -56,22 +77,72 @@ class GameSummRep(ReportLoader):
                     'away': self.__skaters(sks[1])
                 }
 
+                scorer = self.__scorer(scr[5][0])
+
                 # Brian: Fixing issue with syntax, need to revisit the following block of code
                 #        to ensure that the logic is correct
                 assists = []
-                scorer = self.__scorer(scr[5][0])
                 if scorer['num'] in goals[gn]:
                     for s in scr[6:8]:
                         if s and s[0] != u'\xa0':
-                            print s[0], self.__scorer(s[0])
                             assists.append(self.__scorer(s[0]))
                 # EndBrian
 
-                print {
-                    'goal_num': gn,
-                    'scorer': scorer,
-                    'assists': assists
-                }
+                #print {
+                #    'goal_num': gn,
+                #    'scorer': scorer,
+                #    'assists': assists
+                #}
+
+    def parse_goaltender_summary(self):
+        lx_doc = self.html_doc()
+
+        main = lx_doc.xpath('//*[@id="MainTable"]')[0]
+        goal_summ_table = main.xpath('child::tr[15]//tr')
+
+        team = 'away'
+        for row in goal_summ_table:
+            cells = [td.xpath('text()')[0] for td in row.xpath('./td')]
+            if len(cells) == 1: #There's a divider row with 1 cell that separates the teams. Away is always first.
+                team = 'home' #Home from here on out
+
+            if row.get('class') in ['oddColor', 'evenColor']:
+                g = {}
+                g['pos'] = 'G'
+
+                #Parse name + win/loss status
+                name_break = cells[2].replace(',','').split(' ')  # 'LAST, FIRST (W)' -> ['GRUBAUER', 'PHILIPP', '(W)']
+                g['name'] = {'first': name_break[1], 'last': name_break[0]}
+                if len(name_break) == 2:
+                    g['result'] = ''  # Neither win nor loss.
+                else:
+                    g['result'] = name_break[2][1:-1]  # '(W)' -> 'W' // '(OT)' -> 'OT'
+
+                # Build the TOI info
+                g['toi'] = {}
+                for key, val in (('ev', cells[3]), ('pp', cells[4]), ('sh', cells[5]), ('tot', cells[6])):
+                    min, sec = val.split(':') if val != u'\xa0' else ('', '')
+                    g['toi'][key] = {'min': min, 'sec': sec}
+
+                # Tally goals and shots against
+                # QUESTION FOR LATER: What's the appropriate naming convention for the periods (1st, 2nd, 3rd)?
+                # Revise this data structure later.
+                g['ga'] = {}
+                g['sa'] = {}
+
+                kvps = (('per1', cells[7]), ('per2', cells[8]), ('per3', cells[9]), ('tot', cells[10]))
+                if len(cells) == 12:
+                    kvps = (('per1', cells[7]), ('per2', cells[8]), ('per3', cells[9]), ('ot', cells[10]), ('tot', cells[11]))
+
+                # This setup causes an interesting issue, if a goalie faces no shots in OT, stats are reported as if
+                # they never played. See Mrazek in game #194 of 2015-16 season. Do we fill in OT as zero?
+                for key, val in kvps:
+                    if val != u'\xa0':  # Just leave empty if we got an empty cell.
+                        goals, shots = val.split('-')
+                        g['ga'][key] = int(goals)
+                        g['sa'][key] = int(shots)
+
+                self.goalie_summary[team][int(cells[0])] = g
 
     def __period(self, scr):
         period = 0
@@ -105,7 +176,7 @@ class GameSummRep(ReportLoader):
             'seas_tot': to_int(nnt_l[2]) if len(nnt_l) == 3 else -1
         }
 
-    def __skaters(td):
+    def __skaters(self, td):
         sk_d = {}
         for sk in td.xpath('./font'):
             pos_pl = sk.get('title').split(' - ')
