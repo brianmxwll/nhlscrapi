5f89b7aa4187f26049f5e8510410535d064f4bea
Brian Maxwell <brianmxwll@gmail.com>
2015-11-11 10:07:34 -0500

    Whitespace conversion (2 -> 4)
    Fixed tabbing to be consistent with rest of project -- 4 spaces per
    indent instead of 2.

--- nhlscrapi/games/gamesummary.py
+++ nhlscrapi/games/gamesummary.py
@@ -1,4 +1,3 @@
-
 # annoying boilerplate
 # get access to other sub folders
 import sys
@@ -7,12 +6,12 @@ sys.path.append('..')
 from nhlscrapi.scrapr.gamesummrep import GameSummRep
 from nhlscrapi.games.repscrwrap import RepScrWrap
 
+
 class GameSummary(RepScrWrap):
-  
-  def __init__(self, game_key):
-    super(GameSummary, self).__init__(game_key, GameSummRep(game_key))
-    
-    self._summary_rep = GameSummRep(game_key)
-    
-#  @RepScrWrap.read_banner('_roster_rep')
-#  @RepScrWrap.lazy_load('_roster_rep', 'parse_rosters')
+    def __init__(self, game_key):
+        super(GameSummary, self).__init__(game_key, GameSummRep(game_key))
+
+        self._summary_rep = GameSummRep(game_key)
+
+        #  @RepScrWrap.read_banner('_roster_rep')
+        #  @RepScrWrap.lazy_load('_roster_rep', 'parse_rosters')
